c69f18ea3ebaa50d7ec0671d339e08419e1bb4ae
saraang <saraangsk@gmail.com>
2019-11-04 16:13:24 +0530

    Setup project

--- src/main.ts
+++ src/main.ts
@@ -11,5 +11,6 @@ platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
     window['ngRef'].destroy();
   }
   window['ngRef'] = ref;
+  
   // Otherwise, log the boot error
 }).catch(err => console.error(err));
