22c5a0e932e7a0996b61eb7d6a72c9c437d5104d
saraang <saraangsk@gmail.com>
2019-11-06 12:22:29 +0530

    Html pages added

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   if (window['ngRef']) {
     window['ngRef'].destroy();
   }
+
   window['ngRef'] = ref;
   // Otherwise, log the boot error
 }).catch(err => console.error(err));
